c42d9ec91b391879736e23b506760435e3a2cc3b
Luiggy Macias <sosoteluiggy@gmail.com>
2021-03-18 09:40:08 -0500

    updateRecords.js added

---
+++ manipulating-complex-data.js
@@ -0,0 +1,25 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    // Add a record here
+    {
+        "artist": "Billy ",
+        "title": "Piano ",
+        "release_year": 1975,
+        "formats": [
+          "CD",
+          "8T",
+          "LP"
+        ],
+        "gold": true
+      }
+  ];
