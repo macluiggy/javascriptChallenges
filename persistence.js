db063e4ffb39e7792c866048e97cc6254d1d9393
Luiggy Macias <sosoteluiggy@gmail.com>
2021-03-22 11:04:19 -0500

    persistence.js

---
+++ persistence.js
@@ -0,0 +1,19 @@
+function persistence(num) {
+    //code me
+    var count = 0;
+    while (num >= 10) {
+        var digits = num.toString().split('');
+        num = 1;
+        for (let i = 0; i < digits.length; i++) {
+            
+            let digit = parseInt(digits[i]);
+            num *= digit;
+        }
+        count++
+        
+    }
+    console.log(count);
+    return count;
+ }
+
+ persistence(39)
